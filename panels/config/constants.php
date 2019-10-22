c895a4b867a1b49818b50e7edaa6a58b862f5106
zubbey <com.zubbey@icloud.com>
2019-10-22 15:26:34 +0100

    remove all streams system aglorithm

--- panels/config/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('EMAIL', 'coloredigit@gmail.com');
-define('PASSWORD', 'Inno070687');
-
-define('DB_HOST', 'localhost');
-define('DB_USER', 'streams');
-define('DB_PASS', '2019@@Streams');
-define('DB_NAME', 'streamsSystem');
