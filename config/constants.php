3e6b0b5a2f1c154ffdf52cafe892cbc37ac9c542
zubbey <com.zubbey@icloud.com>
2019-10-22 02:18:12 +0100

    add heroku database crediential

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,11 @@
 <?php
-
+// b25cfe20935498
+// 1392dcbd@us-cdbr-iron-east-05.cleardb.net/
+// heroku_d3663816b4ba053
 define('EMAIL', 'coloredigit@gmail.com');
 define('PASSWORD', 'Inno070687');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'Inno070687');
+define('DB_HOST', '1392dcbd@us-cdbr-iron-east-05.cleardb.net/');
+define('DB_USER', 'b25cfe20935498');
+define('DB_PASS', 'heroku_d3663816b4ba053');
 define('DB_NAME', 'streamsSystem');
