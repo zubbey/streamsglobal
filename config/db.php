df2dacef282e41a75bc4d771bf54cf921f439fc9
zubbey <com.zubbey@icloud.com>
2019-10-30 00:31:06 +0100

    made new changes

--- config/db.php
+++ config/db.php
@@ -8,6 +8,7 @@ $WEBSITE_ENVIRONMENT = "Development";
 if(stristr($_SERVER['HTTP_HOST'], 'localhost') === FALSE) $WEBSITE_ENVIRONMENT = "Production";
 // value of variables will change depending on if Development vs Production
 if ($WEBSITE_ENVIRONMENT =="Development") {
+
 	$host 		= "localhost";
 	$user 		= "root";
 	$password 	= "Inno070687";
@@ -17,11 +18,18 @@ if ($WEBSITE_ENVIRONMENT =="Development") {
 	define("APP_BASE_URL", "http://localhost~/zubbey/streamsglobal");
 	error_reporting(E_ALL ^ E_NOTICE); // turn ON showing errors
 } else {
-	$cleardb_url 		= parse_url(getenv("CLEARDB_DATABASE_URL"));
-	$host				= $cleardb_url["us-cdbr-iron-east-05.cleardb.net"];
-	$user 				= $cleardb_url["b5456b157a2fb3"];
-	$password			= $cleardb_url["ac565cd3"];
-	$database 			= substr($cleardb_url["heroku_8fb2f96e9786de5"],1);
+
+	// $cleardb_url 		= parse_url(getenv("CLEARDB_DATABASE_URL"));
+	// $host				= $cleardb_url["us-cdbr-iron-east-05.cleardb.net"];
+	// $user 				= $cleardb_url["b5456b157a2fb3"];
+	// $password			= $cleardb_url["ac565cd3"];
+	// $database 			= substr($cleardb_url["heroku_8fb2f96e9786de5"],1);
+
+	$host 		= "us-cdbr-iron-east-05.cleardb.net";
+	$user 		= "b5456b157a2fb3";
+	$password 	= "ac565cd3";
+	$database 	= "heroku_8fb2f96e9786de5";
+
 	define("APP_ENVIRONMENT", "Production");
 	define("APP_BASE_URL", "http://www.streamsglobal.com");
 	#error_reporting(0); // turn OFF showing errors
