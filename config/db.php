ebb614e38d0426592c0895001bcb13e6b69760c9
zubbey <com.zubbey@icloud.com>
2019-10-29 23:53:35 +0100

    changed jawsDB to clearDB

--- config/db.php
+++ config/db.php
@@ -1,9 +1,8 @@
 <?php
-
 //require ('constants.php');
-require ('jawsdb.php');
+require ('cleardb.php');
 
-$conn = new mysqli($hostname, $username, $password, $database);
+$conn = new mysqli($host, $user, $password, $database);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
--- config/jawsdb.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$url = getenv('JAWSDB_URL');
-$dbparts = parse_url($url);
-
-$hostname = $dbparts['z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
-$username = $dbparts['p4gmndxgwunibq9f'];
-$password = $dbparts['las0bz6giq48d600'];
-$database = ltrim($dbparts['cxqipnmqiw23zt83'],'/');
