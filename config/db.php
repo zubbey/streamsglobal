769a8787f20c1ff5bc5973880169866322596a03
zubbey <com.zubbey@icloud.com>
2019-10-29 23:26:43 +0100

    required php 7.1.3 version

--- config/db.php
+++ config/db.php
@@ -6,5 +6,5 @@ require ('jawsdb.php');
 $conn = new mysqli($hostname, $username, $password, $database);
 
 if ($conn->connect_error) {
-	die('Database error: ' . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
