8803e12f13d9b5675a137d99bbe3f035e7671d7e
zubbey <com.zubbey@icloud.com>
2019-10-30 00:25:40 +0100

    seprated Development Enviroment and Production Enviroment SQL connection

--- config/constants.php
+++ config/constants.php
@@ -3,7 +3,7 @@
 define('EMAIL', 'zubyinnocent@gmail.com');
 define('PASSWORD', 'Inno070687');
 
-define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
-define('DB_USER', 'b5456b157a2fb3');
-define('DB_PASS', 'ac565cd3');
-define('DB_NAME', 'heroku_8fb2f96e9786de5');
+// define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
+// define('DB_USER', 'b5456b157a2fb3');
+// define('DB_PASS', 'ac565cd3');
+// define('DB_NAME', 'heroku_8fb2f96e9786de5');
--- config/db.php
+++ config/db.php
@@ -1,10 +1,37 @@
 <?php
-require ('constants.php');
-//require ('cleardb.php');
+// **********************************************
+// START Database connection and Configuration
+// **********************************************
+// set a default environment
+$WEBSITE_ENVIRONMENT = "Development";
+// detect the URL to determine if it's development or production
+if(stristr($_SERVER['HTTP_HOST'], 'localhost') === FALSE) $WEBSITE_ENVIRONMENT = "Production";
+// value of variables will change depending on if Development vs Production
+if ($WEBSITE_ENVIRONMENT =="Development") {
+	$host 		= "localhost";
+	$user 		= "root";
+	$password 	= "Inno070687";
+	$database 	= "streamsSystem";
 
-//$conn = new mysqli($host, $user, $password, $database);
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+	define("APP_ENVIRONMENT", "Development");
+	define("APP_BASE_URL", "http://localhost~/zubbey/streamsglobal");
+	error_reporting(E_ALL ^ E_NOTICE); // turn ON showing errors
+} else {
+	$cleardb_url 		= parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$host				= $cleardb_url["us-cdbr-iron-east-05.cleardb.net"];
+	$user 				= $cleardb_url["b5456b157a2fb3"];
+	$password			= $cleardb_url["ac565cd3"];
+	$database 			= substr($cleardb_url["heroku_8fb2f96e9786de5"],1);
+	define("APP_ENVIRONMENT", "Production");
+	define("APP_BASE_URL", "http://www.streamsglobal.com");
+	#error_reporting(0); // turn OFF showing errors
+	error_reporting(E_ALL ^ E_NOTICE); // turn ON showing errors
 }
+// connect to the database server
+$conn = mysqli_connect($host, $user, $password) or die("Could not connect to database");
+// select the right database
+mysqli_select_db($conn, $database);
+// **********************************************
+// END Database connection and Configuration
+// **********************************************
+?>
