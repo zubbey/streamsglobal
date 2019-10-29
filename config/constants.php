37d2d4cc6dc81af74fb66bc73671bcd7677f5908
zubbey <com.zubbey@icloud.com>
2019-10-29 22:01:35 +0100

    added HOST PORT to my sqlconnect

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,18 @@
 <?php
-
+// mysql://
+// p4gmndxgwunibq9f
+// :
+// las0bz6giq48d600
+// @
+// z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com
+// :
+// 3306
+// /
+// cxqipnmqiw23zt83
 define('EMAIL', 'zubyinnocent@gmail.com');
 define('PASSWORD', 'Inno070687');
 
-define('DB_HOST', 'z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('DB_HOST', 'z12itfj4c1vgopf8.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306');
 define('DB_USER', 'p4gmndxgwunibq9f');
 define('DB_PASS', 'las0bz6giq48d600');
 define('DB_NAME', 'cxqipnmqiw23zt83');
