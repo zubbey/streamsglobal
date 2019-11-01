0fcf13a7203cf54da630942d959ee27af37e512c
zubbey <com.zubbey@icloud.com>
2019-11-01 21:12:22 +0100

    change sql host

--- config/db.php
+++ config/db.php
@@ -19,7 +19,7 @@ if ($WEBSITE_ENVIRONMENT =="Development") {
 	error_reporting(E_ALL ^ E_NOTICE); // turn ON showing errors
 } else {
 
-	$host 		= "localhost";
+	$host 		= "3.133.95.68";
 	$user 		= "root";
 	$password 	= "Inno070687";
 	$database 	= "streamsSystem";
