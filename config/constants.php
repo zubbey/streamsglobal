3bb72175cd7f347114ef533163351f7a5857a47d
zubbey <com.zubbey@icloud.com>
2019-11-02 10:38:25 +0100

    working on mail test1

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,4 @@
 <?php
 
-define('EMAIL', 'zubyinnocent@gmail.com');
-define('PASSWORD', 'Inno070687');
-
-// define('DB_HOST', 'us-cdbr-iron-east-05.cleardb.net');
-// define('DB_USER', 'b5456b157a2fb3');
-// define('DB_PASS', 'ac565cd3');
-// define('DB_NAME', 'heroku_8fb2f96e9786de5');
+define('EMAIL', 'help@streamsglobal.com');
+define('PASSWORD', '2019@@Streams');
