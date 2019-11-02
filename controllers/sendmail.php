bb8e2f656bba6abb7bd4c3c47850fd004bf7325f
zubbey <com.zubbey@icloud.com>
2019-11-02 11:12:12 +0100

    working on mail test5

--- controllers/sendmail.php
+++ controllers/sendmail.php
@@ -2,7 +2,7 @@
    $to_email = "com.zubbey@hotmail.com";
    $subject = "Simple Email Test via PHP";
    $body = "Hi,nn This is test email send by PHP Script";
-   $headers = "From: help@streamsglobal.com";
+   $headers = "From: coloredigit@gmail.com";
 
    if ( mail($to_email, $subject, $body, $headers)) {
       echo("Email successfully sent to $to_email...");
