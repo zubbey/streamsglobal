ed44913c486a4b317f8413c771fff478201b85ca
zubbey <com.zubbey@icloud.com>
2019-11-09 19:29:18 +0100

    modified about page2

--- about.php
+++ about.php
@@ -38,18 +38,18 @@
     <div class="row text-center d-flex justify-content-center">
       <div class="col">
         <ul class="team_ul">
-          <li class="team_li"><img class="team_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo01.jpg"></li>
-          <li class="team_li"><img class="team_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo02.jpg"></li>
-          <li class="team_li smlist"><img class="team_img sm" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo03.jpg"></li>
-          <li class="team_li"><img class="team_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo04.jpg"></li>
-          <li class="team_li"><img class="team_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo05.jpg"></li>
-          <li class="team_li smlist"><img class="team_img sm" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo06.jpg"></li>
-          <li class="team_li"><img class="team_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo07.jpg"></li>
-          <li class="team_li"><img class="team_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo08.jpg"></li>
-          <li class="team_li smlist"><img class="team_img sm" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo09.jpg"></li>
-          <li class="team_li"><img class="team_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo10.jpg"></li>
-          <li class="team_li"><img class="team_img" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo11.jpg"></li>
-          <li class="team_li smlist"><img class="team_img sm" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo12.jpg"></li>
+          <li class="team_li"><img class="team_img rounded-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo01.jpg"></li>
+          <li class="team_li"><img class="team_img rounded-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo02.jpg"></li>
+          <li class="team_li smlist"><img class="team_img rounded-circle sm" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo03.jpg"></li>
+          <li class="team_li"><img class="team_img rounded-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo04.jpg"></li>
+          <li class="team_li"><img class="team_img rounded-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo05.jpg"></li>
+          <li class="team_li smlist"><img class="team_img rounded-circle sm" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo06.jpg"></li>
+          <li class="team_li"><img class="team_img rounded-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo07.jpg"></li>
+          <li class="team_li"><img class="team_img rounded-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo08.jpg"></li>
+          <li class="team_li smlist"><img class="team_img rounded-circle sm" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo09.jpg"></li>
+          <li class="team_li"><img class="team_img rounded-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo10.jpg"></li>
+          <li class="team_li"><img class="team_img rounded-circle" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo11.jpg"></li>
+          <li class="team_li smlist"><img class="team_img rounded-circle sm" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/10558/photo12.jpg"></li>
         </ul>
       </div>
 
